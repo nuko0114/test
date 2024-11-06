8f6a4f7c151bf86cf94a2cd75931dd78489dbc90
nuko0114 <suzring0629@icloud.com>
2024-11-06 15:03:05 +0900

    TestScriptを変えました！

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
+    public string name = "nuko";
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(name);
     }
 
     // Update is called once per frame
